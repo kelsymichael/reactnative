47428b8816d431f81a5c7268c1aba7080944d795
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-01-16 09:08:40 -0700

    Add app, header, footer
    - Add header & footer.js
    - Remove boilerplate from index.ios.js
    - Verified blank page in sim

---
+++ todo/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class App extends Component {
+  render(){
+    return (
+        <View />
+      );
+  }
+}
+
+export default App;
