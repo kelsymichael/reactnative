9030c5f810051fecf366dd445cd73812e95da119
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-01-17 07:48:18 -0700

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # reactnative
 Quick setup of reactnative - eggheadio
+
+---
+Launching the simulator
+
+- cd into app directory
+* `react-native run-ios`
+or
+* `open ios/nameOfDirectory.xcodeproj/`
+		
--- todo/header.js
+++ todo/header.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
 class Header extends Component {
   render(){
     return (
         <View style={styles.header}>
+          <TouchableOpacity>
+            <Text>{String.fromCharCode(10003)}</Text>
+          </TouchableOpacity>
           <TextInput 
             value={this.props.value}
             onChangeText={this.props.onChange}
